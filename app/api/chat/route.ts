01791db8afc88349845ea7b579052c9a2c176a67
twk <iwasaki.teruyuki@gmail.com>
2024-02-12 12:50:58 +0900

    Add the capability to choose model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
- 
+
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,13 +8,22 @@ const openai = new OpenAI({
  
 // Set the runtime to edge for best performance
 export const runtime = 'edge';
+const ALLOWED_MODELS = ['gpt-3.5-turbo', 'gpt-4-turbo-preview']
  
 export async function POST(req: Request) {
   const { messages } = await req.json();
- 
+  const givenModel = req.headers.get('Model') ?? ''
+
+  if (ALLOWED_MODELS.indexOf(givenModel) < 0) {
+    console.error('model not found=' + givenModel)
+  }
+
+  const model = ALLOWED_MODELS.indexOf(givenModel) >= 0 ? givenModel : ALLOWED_MODELS[0]
+  console.log('model=' + model)
+
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: model,
     stream: true,
     messages,
   });
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 'use client';
  
+import { ChatRequestOptions } from 'ai';
 import { useChat } from 'ai/react';
- 
+
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  const handleChatSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const model = 'gpt-4-turbo-preview'
+    const options:ChatRequestOptions = {options:{headers:{model: model}}}
+
+    e.preventDefault()
+    handleSubmit(e, options)
+  }
+  
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map(m => (
@@ -13,7 +23,7 @@ export default function Chat() {
         </div>
       ))}
  
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleChatSubmit}>
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
