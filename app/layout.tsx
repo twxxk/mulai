b52fc11516ba94d961999cb49ae6bee4965f191b
twk <iwasaki.teruyuki@gmail.com>
2024-02-17 00:26:23 +0900

    fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import DiceLink from "./ui/dicelink";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "MulAI - Multi genAIs",
@@ -19,7 +20,9 @@ export default function RootLayout({
           <a className='flex-1 text-xl hover:text-teal-100 active:text-teal-50' href="/"><h1>
             <span className="font-bold">MulAI</span> - Chat with Multiple genAIs
           </h1></a>
-          <DiceLink className="px-2 hover:text-teal-100 active:text-teal-50" />
+          <Suspense>
+            <DiceLink className="px-2 hover:text-teal-100 active:text-teal-50" />
+          </Suspense>
           <a className='pt-1 text-teal-700' href="https://twitter.com/twk" target="_blank" rel="noopener noreferrer">author: @twk</a>
         </header>
         {children}
